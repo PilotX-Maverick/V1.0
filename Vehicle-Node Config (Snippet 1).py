4c56baa4e8a482c17ae666d7cb7e1378d91447ec
Rizwanullah Khan <reihankhan899@gmail.com>
2023-12-26 13:00:03 +0600

    Change seconday dropdowns to text boxes and Remove extra dropdowns

--- Vehicle-Node Config (Snippet 1).py
+++ Vehicle-Node Config (Snippet 1).py
@@ -1,5 +1,5 @@
-#Do not make any changes in this code snippet, apart from resizing the UI image as instructed in the lines below
-#Before you begin, import "Pillow" (version 9.3.0) package using your in-IDE package installer or through terminal using "pip install Pillow=9.3.0"
+# Do not make any changes in this code snippet, apart from resizing the UI image as instructed in the lines below
+# Before you begin, import "Pillow" (version 9.3.0) package using your in-IDE package installer or through terminal using "pip install Pillow=9.3.0"
 import tkinter as Tk
 import tkinter
 from tkinter import *
@@ -12,30 +12,18 @@ gui.geometry('1250x1080')
 gui.config(bg="grey")
 gui.title('Planner X - Developer 1.0.1')
 
-canvas4 = Canvas(gui, width=1535, height=1100, bg="Black", bd=0, highlightthickness=0) # Resize this and the LINE 19 equally, so as to fit the UI on your entire screen
+canvas4 = Canvas(gui, width=1535, height=1100, bg="Black", bd=0,
+                 highlightthickness=0)  # Resize this and the LINE 19 equally, so as to fit the UI on your entire screen
 gui.state('zoomed')
 start_time5 = time.time()
 canvas4.pack()
 
-img4= Image.open(r'C:\Users\Rezwan\Desktop\safear_internship\Quadcopter.png') # Replace this path with the vehicle's image that you have been assigned for
-img4= img4.resize((1535,800))
+img4 = Image.open(
+    r'C:\Users\Rezwan\Desktop\safear_internship\Quadcopter.png')  # Replace this path with the vehicle's image that you have been assigned for
+img4 = img4.resize((1535, 800))
 img4 = ImageTk.PhotoImage(img4)
 
-chng2 = canvas4.create_image(0,0, anchor=NW, image=img4)
-
-style = ttk.Style(gui)
-style.theme_use('clam')
-
-style.configure('My.TMenubutton', background="grey", foreground='white', borderwidth=1, bordercolor='yellow', focuscolor='none')
-style.map('My.TMenubutton', background=[('active', '#555'), ('pressed', '#333')])
-
-
-slave_devices = ["a", "b", "c"]  # these are just dummy list components, do not worry about them
-
-node_map1_val = StringVar()
-node_map1 = ttk.OptionMenu(canvas4, node_map1_val, "", *slave_devices)
-node_map1.config(width=18) #vary the width as per the need, dont keep too short
-node_map1.place(x=100, y=100) #place the dropdowns on designated spots in a very organised manner with proper alignments
+chng2 = canvas4.create_image(0, 0, anchor=NW, image=img4)
 
 frame_options = ['Select', 'Option 1', 'Option 2', 'Option 3']
 # Positions for dropdowns according to the width and height of the image
@@ -52,7 +40,7 @@ frame_positions = {
 dropdowns = {}
 for name, (x, y) in frame_positions.items():
     var = StringVar(value=frame_options[0])
-    dropdowns[name] = ttk.OptionMenu(gui, var, *frame_options, style='My.TMenubutton')
+    dropdowns[name] = ttk.OptionMenu(gui, var, *frame_options)
     dropdowns[name].config(width=10)
     canvas4.create_window(x, y, window=dropdowns[name])
 
@@ -75,7 +63,7 @@ gimbal_positions = {
 # Place primary gimbal dropdown menus
 for name, options in gimbal_options.items():
     var = StringVar(value=options[0])
-    dropdown = ttk.OptionMenu(gui, var, *options, style='My.TMenubutton')
+    dropdown = ttk.OptionMenu(gui, var, *options)
     dropdown.config(width=10)
     x, y = gimbal_positions[name]
     canvas4.create_window(x, y, window=dropdown)
@@ -83,18 +71,35 @@ for name, options in gimbal_options.items():
 # Define options for the secondary gimbal dropdowns
 secondary_options = ['LL', 'UL']
 
-# Create and place secondary gimbal dropdown menus directly under the primary ones
+
+# Function to clear the default text when the entry gains focus
+def delete_text(event, entry, default_text):
+    if entry.get() == default_text:
+        entry.delete(0, tkinter.END)
+
+
+# Function to re-insert default text
+def insert_default_text(event, entry, default_text):
+    if entry.get().strip() == "":
+        entry.insert(0, default_text)
+
+
+# Place secondary gimbal text boxes directly under the primary ones
 for name, primary_position in gimbal_positions.items():
     primary_x, primary_y = primary_position
-    secondary_y = primary_y + 30  # Y offset from the primary dropdown to place the secondary ones below
+    secondary_y = primary_y + 20  # Y offset from the primary dropdown to place the secondary ones below
 
-    # Create two secondary dropdowns for each primary
-    for i, option in enumerate(secondary_options):
-        var = StringVar(value=option)
-        dropdown = ttk.OptionMenu(gui, var, option, style='My.TMenubutton')
-        dropdown.config(width=2)
-        # Calculate the x position of each secondary dropdown to place them side by side
-        secondary_x = primary_x + (i * 50) - 25  # Adjust the multiplier for spacing, and offset to center
-        canvas4.create_window(secondary_x, secondary_y, window=dropdown)
+    # Create two secondary text boxes for each primary
+    for i, default_text in enumerate(secondary_options):
+        textbox = Entry(canvas4, width=6)
+        secondary_x = primary_x + (i * 50) - 45  # Adjust the multiplier for spacing, and offset to center
+        textbox.place(x=secondary_x, y=secondary_y)
+        textbox.insert(0, default_text)
+
+        # Bind the delete_text function to the focus-in event
+        textbox.bind("<FocusIn>", lambda event, e=textbox, d=default_text: delete_text(event, e, d))
+
+        # Bind the insert_default_text function to the focus-out event
+        textbox.bind("<FocusOut>", lambda event, e=textbox, d=default_text: insert_default_text(event, e, d))
 
 gui.mainloop()
